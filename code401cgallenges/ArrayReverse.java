6be52c6c2935f7064765de990bb4dc4f2ded5b7d
sapanapoudel <sapana.poudel17@outlook.com>
2019-08-06 11:46:02 -0700

    Reverse-array

--- code401cgallenges/ArrayReverse.java
+++ code401cgallenges/ArrayReverse.java
@@ -1,21 +1,41 @@
 import java.util.Arrays;
 
+// import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class ArrayReverse {
   public static void main(String[] args) {
-    System.out.println("Reversed array is: \n");
-    int[] myArray = {1, 2, 3, 4, 5};
+
+    int[] myArray = { 1, 2, 3, 4, 5 };
     reverseArray(myArray, myArray.length);
+
+    int[] testArray = {6, 7, 8, 9};
+    anotherReverseArray(testArray, testArray.length);
+
   }
 
-  public static void reverseArray(int[] arr, int n ) {
-    for (int i = 0; i < n/2; i++) {
-     int initialArray = arr[i];
-     arr[i] = arr[n - 1 - i ];
-     arr[n - 1 - i] = initialArray;
+  // Creating new Array
+  public static void reverseArray(int[] arr, int n) {
+    int[] newArray = new int[n];
+    int j = n;
+    for (int i = 0; i < n; i++) {
+      newArray[j - 1] = arr[i];
+      j = j - 1;
+    }
+    System.out.println("Reverse array is: \n");
 
+    System.out.println(Arrays.toString(newArray));
+
+  }
+
+  //Swapping method in a same array
+  public static void anotherReverseArray(int[] arr, int n){
+    for(int i = 0; i < n/2; i++){
+      int j = arr[i];
+      arr[i] = arr[n - 1 - i];
+      arr[n - 1 - i] = j;
     }
-    for (int j  = 0; j < n; j++){
-      System.out.println(arr[j]);
-    }
+    System.out.println("Reverse array is: \n");
+
+    System.out.println(Arrays.toString(arr));
   }
 }
