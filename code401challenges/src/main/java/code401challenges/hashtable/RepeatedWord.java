af5d4c792afcf16f5ce1d3a8b55975b6400f2d8e
sapanapoudel <sapana.poudel17@outlook.com>
2019-09-16 13:38:23 -0700

    repeated words

--- code401challenges/src/main/java/code401challenges/hashtable/RepeatedWord.java
+++ code401challenges/src/main/java/code401challenges/hashtable/RepeatedWord.java
@@ -1,4 +1,35 @@
 package code401challenges.hashtable;
 
+import com.google.common.base.Strings;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class RepeatedWord {
-}
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+
+    public  HashTable repeatedWord(String str) {
+        HashTable ht = new HashTable(1024);
+        String[] splitWords = str.split(" ");
+        Set<Strings>  words = new HashSet(Arrays.asList(splitWords));
+        int count = 0;
+        for(Strings word :  words) {
+            for(String temp : splitWords) {
+                if(word.equals(temp)) {
+                    ++count;
+                }
+            }
+            ht.add(word, count);
+            count = 0;
+        }
+        return ht;
+
+        }
+
+    }
+
